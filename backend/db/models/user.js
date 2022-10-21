d816966585acf6d2ea0941f11da8dae758a9a57b
Eric Mathews <eam34m@gmail.com>
2022-10-21 14:32:21 -0700

    Attached to Heroku

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -72,10 +72,10 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
-      }
+      },
       firstName: {
         type: DataTypes.STRING
-      }
+      },
       lastName: {
         type: DataTypes.STRING
       }
