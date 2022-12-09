e756506a3b37daa79bd298f111e27a0c8eddfa5e
Eric Mathews <eam34m@gmail.com>
2022-12-09 14:20:29 -0800

    finished phase 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,6 +10,7 @@ import App from './App';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 
 import configureStore from './store';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -18,10 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
